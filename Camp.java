39f52abf63aa0184660639179347c21ef62489f5
roylimxinwei <102473862+roylimxinwei@users.noreply.github.com>
2023-11-25 22:39:23 +0800

    javadoc

--- Camp.java
+++ Camp.java
@@ -1,14 +1,49 @@
+/**
+    Represents a camp created by staff that students can register for as participants or committee members.
+*/
+
 public class Camp {
+    /**
+     * The camps information.
+     */
     private CampInformation campInfo;
 
+    /**
+     * The list of enquries a camp has.
+     */
     private EnquiryList enquiries;
+
+    /**
+     * The list of suggestions a camp has.
+     */
     private SuggestionList suggestions;
-    // private SuggestionList suggestions;
+    
+    /**
+     * The list of students as camp attendees.
+     */
     private StudentList CampAttendeesList;
+
+    /**
+     * The list of students as camp committee members.
+     */
     private StudentList CampCommitteeMembersList;
+
+    /**
+     * The list of students consisting of camp attendees and camp committees.
+     */
     private StudentList CampMemberList;
+
+    /**
+     * The list of students who withdrew from this camp.
+     */
     private StudentList WithdrawnStudentList;
 
+    /**
+     * Creates a new camp with the given campInformation, staffInCharge.
+     * Initialises the following lists: campAttendeesList, CampCommitteeMembersList, enquiries, suggestions, WithdrawnStudentList, CampMemberList.
+     * @param campInformation This is the camp infroamtion.
+     * @param staffInCharge This is the staff in charge of the camp.
+     */
     public Camp(CampInformation campInformation, Staff staffInCharge){
         this.campInfo = campInformation;
         this.campInfo.setStaffInCharge(staffInCharge);
@@ -20,18 +55,50 @@ public class Camp {
         CampMemberList = new StudentList("All Members");
     }
 
-    public CampInformation getCampInfo(){  return this.campInfo;    }
-    public EnquiryList getEnquiries(){ return enquiries;}
-    public SuggestionList getSuggestions(){ return this.suggestions;    }
+    /**
+     * Gets the information of the camp.
+     * @return
+     */
+    public CampInformation getCampInfo(){  return this.campInfo;}
 
+    /**
+     * Gets the enquiry list of the camp.
+     * @return
+     */
+    public EnquiryList getEnquiries(){ return enquiries;}
+
+    /**
+     * Gets the suggestion list of the camp.
+     */
+    public SuggestionList getSuggestions(){ return this.suggestions;}
+
+    /**
+     * Gets the camp Attendee list of the camp.
+     * @return
+     */
     public StudentList getCampAttendeesList() {return CampAttendeesList;}
 
-    public StudentList getCampCommitteeMembersList(){return CampCommitteeMembersList;} 
+    /**
+     * Gets the camp committee list of the camp.
+     * @return
+     */
+    public StudentList getCampCommitteeMembersList(){return CampCommitteeMembersList;}
 
+    /**
+     * Gets the camp attendees and committees list of the camp.
+     * @return
+     */
     public StudentList getCampMembersList(){return this.CampMemberList;}
 
+    /**
+     * Gets the withdrawn student list of the camp.
+     * @return
+     */
     public StudentList getWithdrawnStudentList(){return this.WithdrawnStudentList;}
 
+    /**
+     * Prints the camps name.
+     */
     public void printCampInfo(){
         System.out.println(getCampInfo().getCampName());
     }
--- StaffTextDB.java
+++ StaffTextDB.java
@@ -1,3 +1,4 @@
+
 //read and write textDB (staff)
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -8,112 +9,146 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
+/**
+ * Represents the database interacting with the staff list text file.
+ */
 public class StaffTextDB {
+	/**
+	 * The size of the staff list.
+	 */
 	public static int size;
+	/**
+	 * Seperator.
+	 */
 	public static final String SEPARATOR = "|";
 
-    // an example of reading
+	/**
+	 * Reading the staff list.
+	 * @param filename The name of the file to be read from.
+	 * @return List of staff.
+	 * @throws IOException The exception that gets thrown when file cannot be read.
+	 */
 	public static ArrayList readStaffs(String filename) throws IOException {
 		// read String from text file
-		ArrayList stringArray = (ArrayList)read(filename);
-		ArrayList alr = new ArrayList() ;// to store Professors data
+		ArrayList stringArray = (ArrayList) read(filename);
+		ArrayList alr = new ArrayList();// to store Professors data
 
-        for (int i = 0 ; i < stringArray.size() ; i++) {
-				String st = (String)stringArray.get(i);
-				// get individual 'fields' of the string separated by SEPARATOR
-				StringTokenizer star = new StringTokenizer(st , SEPARATOR);	// pass in the string to the string tokenizer using delimiter ","
+		for (int i = 0; i < stringArray.size(); i++) {
+			String st = (String) stringArray.get(i);
+			// get individual 'fields' of the string separated by SEPARATOR
+			StringTokenizer star = new StringTokenizer(st, SEPARATOR); // pass in the string to the string tokenizer
+																		// using delimiter ","
 
-				String  name = star.nextToken().trim();	// first token
-				String  email = star.nextToken().trim();	// second token
-				Faculty faculty = Faculty.valueOf(star.nextToken().trim());
-				// Faculty faculty = getFacultyFromString(star.nextToken().trim());
-				// 	if (faculty == null) {
-    			// // Handle unknown faculty, e.g., skip this entry, log a warning, etc.
-    			// 		continue;
-				// 	}
-				String password = star.nextToken().trim();
-				//int  faculty = Integer.parseInt(star.nextToken().trim()); // third token
-				// create Professor object from file data
-				Staff staff = new Staff(name, email,password, faculty);
-				// add to Professors list
-				alr.add(staff) ;
-			}
-			return alr ;
+			String name = star.nextToken().trim(); // first token
+			String email = star.nextToken().trim(); // second token
+			Faculty faculty = Faculty.valueOf(star.nextToken().trim());
+			// Faculty faculty = getFacultyFromString(star.nextToken().trim());
+			// if (faculty == null) {
+			// // Handle unknown faculty, e.g., skip this entry, log a warning, etc.
+			// continue;
+			// }
+			String password = star.nextToken().trim();
+			// int faculty = Integer.parseInt(star.nextToken().trim()); // third token
+			// create Professor object from file data
+			Staff staff = new Staff(name, email, password, faculty);
+			// add to Professors list
+			alr.add(staff);
+		}
+		return alr;
 	}
 
+	/**
+	 * Reads all staffs except the specified staff.
+	 * @param filename The name of the file to be read from.
+	 * @param staff The staff to be excluded from being read.
+	 * @return The list of the other staffs
+	 * @throws IOException The exception that gets thrown when file cannot be read.
+	 */
 	public static ArrayList readStaffsExcept(String filename, Staff staff) throws IOException {
 		// read String from text file
-		ArrayList stringArray = (ArrayList)read(filename);
-		ArrayList alr = new ArrayList() ;// to store Professors data
+		ArrayList stringArray = (ArrayList) read(filename);
+		ArrayList alr = new ArrayList();// to store Professors data
 
-        for (int i = 0 ; i < stringArray.size() ; i++) {
-				String st = (String)stringArray.get(i);
-				// get individual 'fields' of the string separated by SEPARATOR
-				StringTokenizer star = new StringTokenizer(st , SEPARATOR);	// pass in the string to the string tokenizer using delimiter ","
+		for (int i = 0; i < stringArray.size(); i++) {
+			String st = (String) stringArray.get(i);
+			// get individual 'fields' of the string separated by SEPARATOR
+			StringTokenizer star = new StringTokenizer(st, SEPARATOR); // pass in the string to the string tokenizer
+																		// using delimiter ","
 
-				String  name = star.nextToken().trim();	// first token
-				String  email = star.nextToken().trim();	// second token
-				//Faculty faculty = getFacultyFromString(star.nextToken().trim());
-				Faculty faculty = Faculty.valueOf(star.nextToken().trim());
-				// 	if (faculty == null) {
-    			// // Handle unknown faculty, e.g., skip this entry, log a warning, etc.
-    			// 		continue;
-				// 	}
+			String name = star.nextToken().trim(); // first token
+			String email = star.nextToken().trim(); // second token
+			// Faculty faculty = getFacultyFromString(star.nextToken().trim());
+			Faculty faculty = Faculty.valueOf(star.nextToken().trim());
+			// if (faculty == null) {
+			// // Handle unknown faculty, e.g., skip this entry, log a warning, etc.
+			// continue;
+			// }
 
-				String password = star.nextToken().trim();
-				//int  faculty = Integer.parseInt(star.nextToken().trim()); // third token
-				// create Professor object from file data
+			String password = star.nextToken().trim();
+			// int faculty = Integer.parseInt(star.nextToken().trim()); // third token
+			// create Professor object from file data
 
-				//add all student except
-				if(staff.getUserID().equals(email)){
-					continue;
-				}
-				Staff readStaff = new Staff(name, email,password, faculty);
-
-				// add to Professors list
-				alr.add(readStaff) ;
+			// add all student except
+			if (staff.getUserID().equals(email)) {
+				continue;
 			}
-			return alr ;
-	}
-	
+			Staff readStaff = new Staff(name, email, password, faculty);
 
-  // an example of saving
-public static void saveStaff(String filename, List al) throws IOException {
-		List alw = new ArrayList() ;// to store Professors data
-
-        for (int i = 0 ; i < al.size() ; i++) {
-				Staff staff = (Staff)al.get(i);
-				StringBuilder st =  new StringBuilder() ;
-				st.append(staff.getName().trim());
-				st.append(SEPARATOR);
-				st.append(staff.getUserID().trim());
-				st.append(SEPARATOR);
-				st.append(staff.getFacultyInformation());
-				st.append(SEPARATOR);
-				st.append(staff.getPassword());
-				alw.add(st.toString()) ;
-			}
-			write(filename,alw);
-	}
-
-  /** Write fixed content to the given file. */
-  public static void write(String fileName, List data) throws IOException  {
-    PrintWriter out = new PrintWriter(new FileWriter(fileName));
-
-    try {
-		for (int i =0; i < data.size() ; i++) {
-      		out.println((String)data.get(i));
+			// add to Professors list
+			alr.add(readStaff);
 		}
-    }
-    finally {
-      out.close();
-    }
-  }
+		return alr;
+	}
 
-  public static void createStaff(Staff createdStaff){
-	StaffTextDB txtDB = new StaffTextDB();
-    	String filename = "stafflist.txt" ;
+	/**
+	 * Saves particular staff to the text file.
+	 * @param filename The name of the file to be read from.
+	 * @param al the list of staff.
+	 * @throws IOException The exception that gets thrown when file cannot be read.
+	 */
+	public static void saveStaff(String filename, List al) throws IOException {
+		List alw = new ArrayList();// to store Professors data
+
+		for (int i = 0; i < al.size(); i++) {
+			Staff staff = (Staff) al.get(i);
+			StringBuilder st = new StringBuilder();
+			st.append(staff.getName().trim());
+			st.append(SEPARATOR);
+			st.append(staff.getUserID().trim());
+			st.append(SEPARATOR);
+			st.append(staff.getFacultyInformation());
+			st.append(SEPARATOR);
+			st.append(staff.getPassword());
+			alw.add(st.toString());
+		}
+		write(filename, alw);
+	}
+
+	/**
+	 * Writes to the staff text file.
+	 * @param fileName The name of the file to be read from.
+	 * @param data the list of the staff.
+	 * @throws IOException The exception that gets thrown when file cannot be read.
+	 */
+	public static void write(String fileName, List data) throws IOException {
+		PrintWriter out = new PrintWriter(new FileWriter(fileName));
+
+		try {
+			for (int i = 0; i < data.size(); i++) {
+				out.println((String) data.get(i));
+			}
+		} finally {
+			out.close();
+		}
+	}
+
+	/**
+	 * Creates a new staff.
+	 * @param createdStaff The staff created.
+	 */
+	public static void createStaff(Staff createdStaff) {
+		StaffTextDB txtDB = new StaffTextDB();
+		String filename = "stafflist.txt";
 		try {
 			// read file containing student records.
 			List al = StaffTextDB.readStaffs(filename);
@@ -122,62 +157,80 @@ public static void saveStaff(String filename, List al) throws IOException {
 			// write Student record/s to file.
 			StaffTextDB.saveStaff(filename, al);
 			size++;
-		}catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("IOException > " + e.getMessage());
 		}
-  }
+	}
 
-  /** Read the contents of the given file. */
-  public static List read(String fileName) throws IOException {
-	List data = new ArrayList() ;
-    Scanner scanner = new Scanner(new FileInputStream(fileName));
-    try {
-      while (scanner.hasNextLine()){
-        data.add(scanner.nextLine());
-      }
-    }
-    finally{
-      scanner.close();
-    }
-    return data;
-  }
+	/**
+	 * Reads the list of staff.
+	 * @param fileName The name of the file to be read from.
+	 * @return List of staff.
+	 * @throws IOException The exception that gets thrown when file cannot be read.
+	 */
+	public static List read(String fileName) throws IOException {
+		List data = new ArrayList();
+		Scanner scanner = new Scanner(new FileInputStream(fileName));
+		try {
+			while (scanner.hasNextLine()) {
+				data.add(scanner.nextLine());
+			}
+		} finally {
+			scanner.close();
+		}
+		return data;
+	}
 
-  public static void populateStaffList(StaffList staffList){
-	StaffTextDB txtDB = new StaffTextDB();
-	String filename = "stafflist.txt" ;
-	try {
-		// read file containing Professor records.
-		ArrayList al = StaffTextDB.readStaffs(filename) ;
-		for (int i = 0 ; i < al.size() ; i++) {
-				Staff staff = (Staff)al.get(i);
+	/**
+	 * Writes to the staff list text file.
+	 * @param staffList The list of staff.
+	 */
+	public static void populateStaffList(StaffList staffList) {
+		StaffTextDB txtDB = new StaffTextDB();
+		String filename = "stafflist.txt";
+		try {
+			// read file containing Professor records.
+			ArrayList al = StaffTextDB.readStaffs(filename);
+			for (int i = 0; i < al.size(); i++) {
+				Staff staff = (Staff) al.get(i);
 				// System.out.println("Name " + student.getName() );
 				// System.out.println("Email " + student.getUserID() );
 				// System.out.println("faculty " + student.getFacultyInformation());
 				// System.out.println("password " + student.getPassword());
-				Staff s = new Staff(staff.getName(),staff.getUserID(),staff.getPassword(), staff.getFacultyInformation());
+				Staff s = new Staff(staff.getName(), staff.getUserID(), staff.getPassword(),
+						staff.getFacultyInformation());
 				staffList.addToList(s);
 				size++;
+			}
+		} catch (IOException e) {
+			System.out.println("IOException > " + e.getMessage());
 		}
-	}catch (IOException e) {
-		System.out.println("IOException > " + e.getMessage());
 	}
-}
-  public static int getSize(){
+
+	/**
+	 * Gets the size of the staff list.
+	 * @return the size of the staff list.
+	 */
+	public static int getSize() {
 		return size;
-}
-
-  public static void updatePassword(Staff staff){
-	StaffTextDB txtDB = new StaffTextDB();
-	String filename = "stafflist.txt" ;
-	try {
-		List al = StaffTextDB.readStaffsExcept(filename, staff) ;
-		al.add(staff);
-		// write Professor record/s to file.
-		StaffTextDB.saveStaff(filename, al);
-	}catch (IOException e) {
-		System.out.println("IOException > " + e.getMessage());
 	}
 
-}
+	/**
+	 * Updates the password of the staff in text file.
+	 * @param staff The staffs password to be change.
+	 */
+	public static void updatePassword(Staff staff) {
+		StaffTextDB txtDB = new StaffTextDB();
+		String filename = "stafflist.txt";
+		try {
+			List al = StaffTextDB.readStaffsExcept(filename, staff);
+			al.add(staff);
+			// write Professor record/s to file.
+			StaffTextDB.saveStaff(filename, al);
+		} catch (IOException e) {
+			System.out.println("IOException > " + e.getMessage());
+		}
+
+	}
 
 }
